3233fd9a248d4887a07e7f71b89a636883536872
Arthur Woimbée <arthur.woimbee@meero.com>
2019-09-25 17:18:04 +0200

    better norme

--- src/ParamsToEnvCommand.php
+++ src/ParamsToEnvCommand.php
@@ -1,15 +1,17 @@
 <?php
+
 // src/Command/ParamsToEnvCommand.php
+
 namespace Commands;
 
+use \in_array;
+use \is_array;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Yaml\Yaml;
-use \in_array;
-use \is_array;
 
 class ParamsToEnvCommand extends Command
 {
@@ -18,27 +20,25 @@ class ParamsToEnvCommand extends Command
     protected function configure()
     {
         $this
-            ->setDescription("TODO")
-            ->addOption('input', 'i', InputOption::VALUE_OPTIONAL, 'The file to translate', 'php://stdin')
-            ->addOption('output', 'o', InputOption::VALUE_OPTIONAL, 'The file to output to', 'php://stdout')
-            ->addOption('exclusions', 'e', InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'The parameters to exclude (lowercase)', array())
+            ->setDescription('Read a .yml parameter file and outputs the \'parameters:\' to a .env file (or stdout)')
+            ->addArgument('input', InputArgument::REQUIRED, 'The file to translate')
+            ->addOption('output', 'o', InputOption::VALUE_REQUIRED, 'The file to output to', 'php://stdout')
+            ->addOption('exclusions', 'e', InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'The parameters to exclude (lowercase)', [])
             ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $paramsFile = $input->getOption('input');
-        $envFile = $input->getOption('output');
+        $yamlFile =   $input->getArgument('input');
+        $envFile =    $input->getOption('output');
         $exclusions = $input->getOption('exclusions');
 
-        $yamlData = Yaml::parseFile($paramsFile);
+        $yamlData = Yaml::parseFile($yamlFile);
         $data = '';
-        foreach(($yamlData['parameters']) as $keyYml => $param) {
+        foreach ($yamlData['parameters'] as $keyYml => $param) {
             $keyEnv = str_replace('.', '_', strtoupper($keyYml));
-            if (
-                !in_array($keyYml, $exclusions)
-                && !in_array($keyEnv, $exclusions)
-            ) {
+            if (!in_array($keyYml, $exclusions)
+                && !in_array($keyEnv, $exclusions)) {
                 if (is_array($param)) {
                     $param = json_encode($param);
                 }
--- src/PrintEnvCommand.php
+++ src/PrintEnvCommand.php
@@ -1,11 +1,11 @@
 <?php
+
 // src/Command/PrintEnvCommand.php
+
 namespace Commands;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Dotenv\Dotenv;
 
@@ -15,14 +15,13 @@ class PrintEnvCommand extends Command
 
     protected function configure()
     {
-        $this->setDescription("Print env variables as read by symfonys Dotenv");
+        $this->setDescription('Print env variables as read by symfonys Dotenv');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $a = new Dotenv();
         $a->loadEnv(__DIR__ . '/../.env');
-		print_r($_ENV);
-        return ;
+        print_r($_ENV);
     }
 }
